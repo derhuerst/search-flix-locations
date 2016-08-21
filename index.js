96560f9b074f37f6040c654a8e2e56ece6a8946a
Jannis R <mail@jannisr.de>
2016-08-21 14:00:14 +0200

    implementation, part 1 :clap:

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 'use strict'
 
-// todo
+const hifo = require('hifo')
+
+const tokenize = require('./tokenize')
+
+const byId = require('./by-id.json')
+const byToken = require('./by-token.json')
+
+
+
+const findTokensForFragment = (fragment) => {
+	// exact match
+	if (fragment in byToken) return [{
+		name: fragment, relevance: Math.sqrt(fragment.length)
+	}]
+
+	const results = []
+	for (let token in byToken) {
+		// match begins with fragment
+		if (fragment === token.slice(0, fragment.length)) results.push({
+			name: token, relevance: fragment.length / token.length
+		})
+	}
+
+	return results
+}
+
+const enrichTokenWithStations = (token) => {
+	token.stations = []
+	for (let id of byToken[token.name])
+		token.stations.push(byId[id])
+	return token
+}
+
+const enrichFragmentWithTokens = (fragment) => ({
+	name: fragment,
+	tokens: findTokensForFragment(fragment).map(enrichTokenWithStations)
+})
+
+
+
+const autocomplete = (query, limit) => {
+	if (query.trim() === '') return []
+	const results = hifo(hifo.highest('relevance'), limit || 5)
+
+	const fragments = tokenize(query).map(enrichFragmentWithTokens)
+
+	return fragments
+}
+
+module.exports = autocomplete
