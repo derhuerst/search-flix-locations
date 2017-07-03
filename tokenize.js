bcbc03d58b6e48a018aec562e162b19d155fb5cf
Jannis R <mail@jannisr.de>
2017-07-04 01:56:52 +0200

    normalize-for-search -> slug
    because of Unicode chars

--- tokenize.js
+++ tokenize.js
@@ -1,12 +1,13 @@
 'use strict'
 
-const normalize = require('normalize-for-search')
+const normalize = require('slug')
 
 const delim = /[\s\/\(\)\-,\.\+]+/
 
 // todo: see derhuerst/vbb-tokenize-station
 const tokenize = (name) =>
 	normalize(name)
+	.toLowerCase()
 	.split(delim)
 	.map((part) => part.trim())
 	.filter((part) => part.length > 0)
