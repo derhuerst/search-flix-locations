fe42834615fdc648571cc9c1ff0b9ea412435c09
Jannis R <mail@jannisr.de>
2018-05-12 17:24:25 +0200

    example: cli-autocomplete

--- example.js
+++ example.js
@@ -1,6 +1,32 @@
 'use strict'
 
-const pick = require('lodash.pick')
-const search = require('.')
+const prompt = require('cli-autocomplete')
 
-console.log(search('berlin', 5))
+const autocomplete = require('.')
+
+const suggest = (input) => {
+	const results = autocomplete(input, 5)
+	const choices = []
+
+	for (let result of results) {
+		choices.push({
+			title: [
+				result.name,
+				'–',
+				'score:', result.score.toFixed(3),
+				'relevance:', result.relevance.toFixed(3)
+			].join(' '),
+			value: result.id
+		})
+	}
+
+	return Promise.resolve(choices)
+}
+
+prompt('Type a station name!', suggest)
+.once('abort', () => {
+	process.exitCode = 1
+})
+.once('submit', (id) => {
+	console.log('station ID:', id)
+})
